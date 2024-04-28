b16dd7fefa96e7e2c88405b3613d045a58218e29
Abdalrhman Awad <abed.awad210@gmail.com>
2024-04-28 15:02:08 +0300

    كيف الصحة

---
+++ app/src/main/java/HUS/REMAS/remas11firstproj/SplashScreen.java
@@ -0,0 +1,24 @@
+package HUS.REMAS.remas11firstproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+    private TextView tv_Output;
+    private Button btn_Go;
+    private EditText et_Input;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        //koko wawaبغفاابال
+    }
+}
