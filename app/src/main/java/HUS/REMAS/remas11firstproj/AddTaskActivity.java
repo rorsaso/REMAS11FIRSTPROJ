97ba950564a81efbf2881106de7c1c6b6b6e1244
Abdalrhman Awad <abed.awad210@gmail.com>
2024-04-30 11:49:06 +0300

    كيف الصحة

---
+++ app/src/main/java/HUS/REMAS/remas11firstproj/AddTaskActivity.java
@@ -0,0 +1,14 @@
+package HUS.REMAS.remas11firstproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+    }
+}
