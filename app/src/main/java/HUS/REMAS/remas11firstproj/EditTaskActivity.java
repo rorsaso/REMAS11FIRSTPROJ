f952e697861e27052a2c2b74456dedc017884169
Abdalrhman Awad <abed.awad210@gmail.com>
2024-05-12 10:49:18 +0300

    كيف الصحة

---
+++ app/src/main/java/HUS/REMAS/remas11firstproj/EditTaskActivity.java
@@ -0,0 +1,14 @@
+package HUS.REMAS.remas11firstproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_task);
+    }
+}
